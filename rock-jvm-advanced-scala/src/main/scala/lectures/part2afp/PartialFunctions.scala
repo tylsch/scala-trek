a1bf4a795621c79c515e104efe460e70a4bd1602
Tyler Schock <schock3736@gmail.com>
2022-07-20 19:30:08 -0500

    Rock the JVM Advanced Scala - Partial Functions Complete

---
+++ rock-jvm-advanced-scala/src/main/scala/lectures/part2afp/PartialFunctions.scala
@@ -0,0 +1,82 @@
+package lectures.part2afp
+
+object PartialFunctions extends App {
+  val aFunction = (x: Int) => x + 1 // Function1[Int, Int] === Int => Int
+  val aFussyFunction = (x: Int) =>
+    if (x == 1) 42
+    else if (x == 2) 56
+    else if (x == 5) 999
+    else throw new FunctionApplicableException
+
+  class FunctionApplicableException extends RuntimeException
+
+  val aNicerFussyFunction = (x: Int) => x match {
+    case 1 => 42
+    case 2 => 56
+    case 5 => 999
+  }
+  // {1,2,5} => Int
+
+  val aPartialFunction: PartialFunction[Int, Int] = {
+    case 1 => 42
+    case 2 => 56
+    case 5 => 999
+  } // Partial Function Value == aNicerFussyFunction
+
+  println(aPartialFunction(2))
+  //println(aPartialFunction(45)) // Match Error
+
+  // PF Utilities
+  println(aPartialFunction.isDefinedAt(67))
+
+  // Lift
+  val lifted = aPartialFunction.lift // Int => Option[Int]
+  println(lifted(2))
+  println(lifted(98))
+
+  val pfChain = aPartialFunction.orElse[Int, Int] {
+    case 45 => 67
+  }
+  println(pfChain(2))
+  println(pfChain(45))
+
+  // PF extend normal functions
+  val aTotalFunction: Int => Int = {
+    case 1 => 99
+  }
+
+  // HOFs accept partial functions as well
+  val aMappedList = List(1,2,3).map {
+    case 1 => 42
+    case 2 => 78
+    case 3 => 1000
+  }
+  println(aMappedList)
+
+  /*
+  * Note: PF can only have ONE parameter type
+  * */
+
+  /*
+  * Exercises
+  * 1) construct a PF instance your self
+  * 2) dumb chatbot as a PF
+  * */
+
+  val aManualFussyFunction = new PartialFunction[Int, Int] {
+    override def apply(v1: Int): Int = v1 match {
+      case 1 => 42
+      case 2 => 65
+      case 5 => 999
+    }
+
+    override def isDefinedAt(x: Int): Boolean =
+      x == 1 || x == 2 || x == 5
+  }
+  val chatbot: PartialFunction[String, String] = {
+    case "hello" => "Hi, my name is HAL9000"
+    case "goodbye" => "Once you start talking to me, there is no return, human!"
+    case "call mom" => "Unable to find your phone without your credit car"
+  }
+  scala.io.Source.stdin.getLines().map(chatbot).foreach(println)
+}
