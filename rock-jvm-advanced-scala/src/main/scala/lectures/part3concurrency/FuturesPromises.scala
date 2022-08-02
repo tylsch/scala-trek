90b26d6c4da2126eb39226f969978371e75e936c
Tyler Schock <schock3736@gmail.com>
2022-08-01 19:21:49 -0500

    Rock the JVM Advanced Scala - Futures and Promises Part 3 exercises Complete

--- rock-jvm-advanced-scala/src/main/scala/lectures/part3concurrency/FuturesPromises.scala
+++ rock-jvm-advanced-scala/src/main/scala/lectures/part3concurrency/FuturesPromises.scala
@@ -1,7 +1,9 @@
 package lectures.part3concurrency
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future, Promise}
+import scala.language.postfixOps
 import scala.util.{Failure, Random, Success}
 
 object FuturesPromises extends App {
@@ -51,40 +53,93 @@ object FuturesPromises extends App {
       val bfId = friends(profile.id)
       Profile(bfId, names(bfId))
     }
-
-    // Client: Mark to poke Bill
-    val mark = SocialNetwork.fetchProfile("fb.id.1-zuck")
-//    mark.onComplete {
-//      case Success(markProfile) => {
-//        val bill = SocialNetwork.fetchBestFriend(markProfile)
-//        bill.onComplete {
-//          case Success(billProfile) => markProfile.poke(billProfile)
-//          case Failure(exception) => exception.printStackTrace()
-//        }
-//      }
-//      case Failure(exception) => exception.printStackTrace()
-//    }
-
-    val nameOnTheWall: Future[String] = mark.map(profile => profile.name)
-    val marksBestFriend: Future[Profile] = mark.flatMap(profile => SocialNetwork.fetchBestFriend(profile))
-    val zucksBestFriendRestricted: Future[Profile] = marksBestFriend.filter(profile => profile.name.startsWith("Z"))
-
-    for {
-      mark <- SocialNetwork.fetchProfile("fb.id.1-zuck")
-      bill <- SocialNetwork.fetchBestFriend(mark)
-    } mark.poke(bill)
-
-    Thread.sleep(2000)
-
-    // Fallbacks
-    val aProfileNoMatterWhat = SocialNetwork.fetchProfile("unknown-id").recover {
-      case e: Throwable => Profile("fb.id.0-dummy", "Forever Alone")
-    }
-
-    val aFetchedProfileNoMatterWhat = SocialNetwork.fetchProfile("unknown").recoverWith {
-      case e: Throwable => SocialNetwork.fetchProfile("fb.id.0-dummy")
-    }
-
-    val fallbackResult = SocialNetwork.fetchProfile("unknown").fallbackTo(SocialNetwork.fetchProfile("fb.id.0-dummy"))
   }
+
+  // Client: Mark to poke Bill
+  val mark = SocialNetwork.fetchProfile("fb.id.1-zuck")
+  //    mark.onComplete {
+  //      case Success(markProfile) => {
+  //        val bill = SocialNetwork.fetchBestFriend(markProfile)
+  //        bill.onComplete {
+  //          case Success(billProfile) => markProfile.poke(billProfile)
+  //          case Failure(exception) => exception.printStackTrace()
+  //        }
+  //      }
+  //      case Failure(exception) => exception.printStackTrace()
+  //    }
+
+  val nameOnTheWall: Future[String] = mark.map(profile => profile.name)
+  val marksBestFriend: Future[Profile] = mark.flatMap(profile => SocialNetwork.fetchBestFriend(profile))
+  val zucksBestFriendRestricted: Future[Profile] = marksBestFriend.filter(profile => profile.name.startsWith("Z"))
+
+  for {
+    mark <- SocialNetwork.fetchProfile("fb.id.1-zuck")
+    bill <- SocialNetwork.fetchBestFriend(mark)
+  } mark.poke(bill)
+
+  Thread.sleep(2000)
+
+  // Fallbacks
+  val aProfileNoMatterWhat = SocialNetwork.fetchProfile("unknown-id").recover {
+    case e: Throwable => Profile("fb.id.0-dummy", "Forever Alone")
+  }
+
+  val aFetchedProfileNoMatterWhat = SocialNetwork.fetchProfile("unknown").recoverWith {
+    case e: Throwable => SocialNetwork.fetchProfile("fb.id.0-dummy")
+  }
+
+  val fallbackResult = SocialNetwork.fetchProfile("unknown").fallbackTo(SocialNetwork.fetchProfile("fb.id.0-dummy"))
+
+  // online banking app
+  case class User(name: String)
+
+  case class Transaction(sender: String, receiver: String, amount: Double, status: String)
+
+  object BankingApp {
+    val name = "Rock the JVM Banking"
+
+    def fetchUser(name: String): Future[User] = Future {
+      Thread.sleep(500)
+      User(name)
+    }
+
+    def createTransaction(user: User, merchantName: String, amount: Double): Future[Transaction] = Future {
+      Thread.sleep(1000)
+      Transaction(user.name, merchantName, amount, "Success")
+    }
+
+    def purchase(username: String, item: String, merchantName: String, cost: Double): String = {
+      // fetch user from the DB
+      // create transaction
+      // WAIT for the transaction to finish
+
+      val status = for {
+        user <- fetchUser(username)
+        transaction <- createTransaction(user, merchantName, cost)
+      } yield transaction.status
+
+      Await.result(status, 2 seconds) // implicit conversions -> pimp my library
+    }
+  }
+
+  println(BankingApp.purchase("Daniel", "iPhone 14", "Rock the JVM Store", 3000))
+
+  // promises
+  val promise = Promise[Int]() // "controller" over a future
+  val future = promise.future
+
+  // Thread 1 - "consumer"
+  future.onComplete {
+    case Success(value) => println(s"[consumer] I've received $value")
+  }
+
+  val producer = new Thread(() => {
+    println("[producer] crunching numbers...")
+    Thread.sleep(1000)
+    promise.success(42)
+    println("[producer] done")
+  })
+
+  producer.start()
+  Thread.sleep(1000)
 }
