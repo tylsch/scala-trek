038541ae71e72d5e8e1e604962f06603bfab187d
Tyler Schock <schock3736@gmail.com>
2022-08-25 15:10:39 -0500

    Rock the JVM Spark Essentials - Investigative Data Analysis on Big Data Exercise Complete

--- rock-jvm-spark-essentials/src/main/scala/part7bigdata/TaxiApplication.scala
+++ rock-jvm-spark-essentials/src/main/scala/part7bigdata/TaxiApplication.scala
@@ -1,6 +1,7 @@
 package part7bigdata
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Column, SparkSession}
+import org.apache.spark.sql.functions._
 
 object TaxiApplication extends App {
   val spark = SparkSession.builder()
@@ -17,4 +18,63 @@ object TaxiApplication extends App {
     .csv("src/main/resources/data/taxi_zones.csv")
 
   taxiZonesDF.printSchema()
+
+  val pickupsByTaxiZoneDF = taxiDF.groupBy("PULocationID")
+    .agg(count("*").as("totalTrips"))
+    .join(taxiZonesDF, col("PULocationID") === col("LocationID"))
+    .drop("LocationID", "service_zone")
+    .orderBy(col("totalTrips").desc_nulls_last)
+
+
+  // 1b - group by borough
+  val pickupsByBorough = pickupsByTaxiZoneDF.groupBy(col("Borough"))
+    .agg(sum(col("totalTrips")).as("totalTrips"))
+    .orderBy(col("totalTrips").desc_nulls_last)
+
+  // 2
+  val pickupsByHourDF = taxiDF
+    .withColumn("hour_of_day", hour(col("tpep_pickup_datetime")))
+    .groupBy("hour_of_day")
+    .agg(count("*").as("totalTrips"))
+    .orderBy(col("totalTrips").desc_nulls_last)
+
+  // 3
+  val tripDistanceDF = taxiDF.select(col("trip_distance").as("distance"))
+  val longDistanceThreshold = 30
+  val tripDistanceStatsDF = tripDistanceDF.select(
+    count("*").as("count"),
+    lit(longDistanceThreshold).as("threshold"),
+    mean("distance").as("mean"),
+    stddev("distance").as("stddev"),
+    min("distance").as("min"),
+    max("distance").as("max")
+  )
+
+  val tripsWithLengthDF = taxiDF
+    .withColumn("isLong", col("trip_distance") >= longDistanceThreshold)
+  val tripsByLengthFD = tripsWithLengthDF.groupBy("isLong").count()
+
+  // 4
+  val pickupsByHourByLengthDF = tripsWithLengthDF
+    .withColumn("hour_of_day", hour(col("tpep_pickup_datetime")))
+    .groupBy("hour_of_day", "isLong")
+    .agg(count("*").as("totalTrips"))
+    .orderBy(col("totalTrips").desc_nulls_last)
+
+  // 5
+  def pickupDropOffPopularity(predicate: Column) = tripsWithLengthDF
+    .where(predicate)
+    .groupBy("PULocationID", "DOLocationID")
+    .agg(count("*").as("totalTrips"))
+    .join(taxiZonesDF, col("PULocationID") === col("LocationID"))
+    .withColumnRenamed("Zone", "Pickup_Zone")
+    .drop("LocationID", "Borough", "service_zone")
+    .join(taxiZonesDF, col("DOLocationID") === col("LocationID"))
+    .withColumnRenamed("Zone", "Dropoff_Zone")
+    .drop("LocationID", "Borough", "service_zone")
+    .drop("PULocationID", "DOLocationID")
+    .orderBy(col("totalTrips").desc_nulls_last)
+
+  pickupDropOffPopularity(col("isLong")).show()
+  pickupDropOffPopularity(not(col("isLong"))).show()
 }
