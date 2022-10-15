a3bddb41f255b71b8cc80c67f45274efba931848
Tyler Schock <schock3736@gmail.com>
2022-10-15 15:16:28 -0500

    Completed Synchronous Testing Exercise - Rock the JVM Akka Typed Essentials

---
+++ rock-jvm-akka-essentials/src/test/scala/part3testing/SynchronousTestingSpec.scala
@@ -0,0 +1,43 @@
+package part3testing
+
+import akka.actor.testkit.typed.CapturedLogEvent
+import akka.actor.testkit.typed.Effect.Spawned
+import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, ScalaTestWithActorTestKit, TestInbox}
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.slf4j.event.Level
+
+import scala.Seq
+
+class SynchronousTestingSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+  import part2actors.ChildActorsExercise._
+  "A word counter master" should {
+    "spawn a child upon reception of the initialize message" in {
+      val master = BehaviorTestKit(WordCounterMaster())
+      master.run(Initialize(1)) // synchronous "sending" of the message Initialize(1)
+
+      // check that an effect was produced
+      val effect = master.expectEffectType[Spawned[WorkerProtocol]]
+      // inspect the contents of those effects
+      effect.childName should equal("worker1")
+    }
+
+    "send a task to a child" in {
+      val master = BehaviorTestKit(WordCounterMaster())
+      master.run(Initialize(1))
+      val effect = master.expectEffectType[Spawned[WorkerProtocol]]
+
+      val mailbox = TestInbox[UserProtocol]() // the "requesters" inbox
+      // start processing
+      master.run(WordCountTask("Akka testing is pretty powerful", mailbox.ref))
+      // mock the reply from the child
+      master.run(WordCountReply(0, 5))
+      // test that the requester got the right message
+      mailbox.expectMessage(Reply(5))
+    }
+    "log messages" in {
+      val master = BehaviorTestKit(WordCounterMaster())
+      master.run(Initialize(1))
+      master.logEntries() shouldBe Seq(CapturedLogEvent(Level.INFO, "[master] initializing with 1 children"))
+    }
+  }
+}
