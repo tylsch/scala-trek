03b10d353c1f6174d9cb1e48780ff2071353fa72
Tyler Schock <schock3736@gmail.com>
2022-10-25 06:53:16 -0500

    The GraphStage API Part 2 :: Complete :: Rock the JVM Akka Streams

--- rock-jvm-akka-streams/src/main/scala/part5advanced/CustomOperators.scala
+++ rock-jvm-akka-streams/src/main/scala/part5advanced/CustomOperators.scala
@@ -1,12 +1,13 @@
 package part5advanced
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
-import akka.stream.{Attributes, Inlet, Outlet, SinkShape, SourceShape}
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet, SinkShape, SourceShape}
+import akka.stream.stage.{GraphStage, GraphStageLogic, GraphStageWithMaterializedValue, InHandler, OutHandler}
 
 import scala.collection.mutable
-import scala.util.Random
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Random, Success}
 
 object CustomOperators extends App {
   implicit val system: ActorSystem = ActorSystem("CustomOperators")
@@ -73,5 +74,93 @@ object CustomOperators extends App {
   }
 
   val batcherSink = Sink.fromGraph(new Batcher(10))
-  randomNumberGeneratorSource.to(batcherSink).run()
+  //randomNumberGeneratorSource.to(batcherSink).run()
+
+  class SimpleFilter[T](predicate: T => Boolean) extends GraphStage[FlowShape[T, T]] {
+    val inPort = Inlet[T]("filterIn")
+    val outPort = Outlet[T]("filterOut")
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+      setHandler(outPort, new OutHandler {
+        override def onPull(): Unit = pull(inPort)
+      })
+      setHandler(inPort, new InHandler {
+        override def onPush(): Unit = {
+          try {
+            val nextElement = grab(inPort)
+
+            if (predicate(nextElement))
+              push(outPort, nextElement)
+            else
+              pull(inPort)
+          }
+          catch {
+            case e: Throwable => failStage(e)
+          }
+        }
+      })
+    }
+    override def shape: FlowShape[T, T] = FlowShape(inPort, outPort)
+  }
+
+  val myFilter = Flow.fromGraph(new SimpleFilter[Int](_ > 50))
+  //randomNumberGeneratorSource.via(myFilter).to(batcherSink).run()
+
+  /*
+  * Materialized Values in Graph Stages
+  * */
+
+  // 3 - a flow that counts the number of elements that goes through it
+  class CounterFlow[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], Future[Int]] {
+    val inPort: Inlet[T] = Inlet[T]("counterIn")
+    val outPort: Outlet[T] = Outlet[T]("counterOut")
+    override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[Int]) = {
+      val promise = Promise[Int]
+      val logic: GraphStageLogic = new GraphStageLogic(shape) {
+        // setting mutable state
+        var counter = 0
+
+        setHandler(outPort, new OutHandler {
+          override def onPull(): Unit = pull(inPort)
+
+          override def onDownstreamFinish(cause: Throwable): Unit = {
+            promise.success(counter)
+            super.onDownstreamFinish(cause)
+          }
+        })
+        setHandler(inPort, new InHandler {
+          override def onPush(): Unit = {
+            val nextElement = grab(inPort)
+            counter += 1
+            push(outPort, nextElement)
+          }
+          override def onUpstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onUpstreamFinish()
+          }
+          override def onUpstreamFailure(ex: Throwable): Unit = {
+            promise.failure(ex)
+            super.onUpstreamFailure(ex)
+          }
+        })
+      }
+
+      (logic, promise.future)
+    }
+    override val shape: FlowShape[T, T] = FlowShape(inPort, outPort)
+  }
+
+  val counterFlow = Flow.fromGraph(new CounterFlow[Int])
+  val countFuture = Source(1 to 10)
+    //.map(x => if (x == 7) throw new RuntimeException else x)
+    .viaMat(counterFlow)(Keep.right)
+    //.to(Sink.foreach(x => if (x == 7) throw new RuntimeException else println(x)))
+    .to(Sink.foreach[Int](println))
+    .run()
+
+  import system.dispatcher
+  countFuture.onComplete {
+    case Success(value) => println(s"The number of elements passed: $value")
+    case Failure(exception) => println(s"Counting the elements failed: $exception")
+  }
 }
