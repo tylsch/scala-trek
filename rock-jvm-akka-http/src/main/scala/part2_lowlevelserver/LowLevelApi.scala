da444b758305fb38cfdba2a462b5916bd00d3b8f
Tyler Schock <schock3736@gmail.com>
2022-10-31 09:04:42 -0500

    The Low-Level Server API - Part 2 :: Akka HTTP with Scala :: Rock the JVM

--- rock-jvm-akka-http/src/main/scala/part2_lowlevelserver/LowLevelApi.scala
+++ rock-jvm-akka-http/src/main/scala/part2_lowlevelserver/LowLevelApi.scala
@@ -3,6 +3,7 @@ package part2_lowlevelserver
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.IncomingConnection
+import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest, HttpResponse, StatusCodes, Uri}
 import akka.stream.scaladsl.{Flow, Sink}
 
@@ -155,6 +156,56 @@ object LowLevelApi extends App {
 //    connection.handleWith(streamsBasedRequestHandler)
 //  }
   // shorthand version
-  Http().newServerAt("localhost", 8082).bindFlow(streamsBasedRequestHandler)
+  //Http().newServerAt("localhost", 8082).bindFlow(streamsBasedRequestHandler)
+
+  /*
+  * Exercise: create your own HTTP Server running on localhost on 8388, which replies
+  * - with a welcome message on the "front door" localhost:8388
+  * - with a proper HTML on localhost:8388/about
+  * - with a 404 message otherwise
+  * */
+
+  val syncExerciseHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(HttpMethods.GET, Uri.Path("/"), _, _, _) =>
+      HttpResponse(
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          "Hello from the exercise front door!"
+        )
+      )
+    case HttpRequest(HttpMethods.GET, Uri.Path("/search"), _, _, _) =>
+      HttpResponse(
+        StatusCodes.Found,
+        headers = List(Location("https://google.com"))
+      )
+    case HttpRequest(HttpMethods.GET, Uri.Path("/about"), _, _, _) =>
+      HttpResponse(
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |  <body>
+            |    <div style="color: red">Hello from about page!</div>
+            |  </body>
+            |</html>
+            |""".stripMargin
+        )
+      )
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      HttpResponse(
+        StatusCodes.NotFound,
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          "OOPS, you're in no man's land, sorry"
+        )
+      )
+  }
+
+  val bindingFuture = Http().newServerAt("localhost", 8388).bindSync(syncExerciseHandler)
+  // shutdown server
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
 
 }
