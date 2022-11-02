e29b6c3ced96d447cfd84295994be7f5b6788b17
Tyler Schock <schock3736@gmail.com>
2022-11-02 07:28:25 -0500

    Directives Breakdown Part 2 :: Akka HTTP with Scala :: Rock the JVM

--- rock-jvm-akka-http/src/main/scala/part3_highlevelserver/DirectivesBreakdown.scala
+++ rock-jvm-akka-http/src/main/scala/part3_highlevelserver/DirectivesBreakdown.scala
@@ -88,6 +88,88 @@ object DirectivesBreakdown extends App {
       }
     }
 
+  /*
+  * Type #3: Composite Directives
+  * */
+
+  val simpleNestedRoute: Route =
+    path("api" / "item") {
+      get {
+        complete(StatusCodes.OK)
+      }
+    }
+
+  val compactSimpleNestedRoute = (path("api" / "item") & get) {
+    complete(StatusCodes.OK)
+  }
+
+  val compactExtractRequestRoute: Route =
+    (path("controlEndpoint") & extractRequest & extractLog) {
+      (request: HttpRequest, log: LoggingAdapter) =>
+        log.info(s"I got the HTTP request: $request")
+        complete(StatusCodes.OK)
+    }
+
+  // /about and /aboutUs
+  val repeatedRoute: Route =
+    path("about") {
+      complete(StatusCodes.OK)
+    } ~
+    path ("aboutUs") {
+      complete(StatusCodes.OK)
+    }
+
+  val dryRoute: Route =
+    (path("about") | path("aboutUs")) {
+      complete(StatusCodes.OK)
+    }
+
+  // yourblog.com/42 AND yourblog.com?postId=42
+  val blogByIdRoute: Route = {
+    path(IntNumber) { (blogId: Int) =>
+      complete(StatusCodes.OK)
+    }
+  }
+
+  val blogByQueryParamRoute: Route =
+    parameter("postId".as[Int]) { (blogPostId: Int) =>
+      complete(StatusCodes.OK)
+    }
+
+  val combinedBlogByIdRoute: Route =
+    (path(IntNumber) | parameter("postId".as[Int])) { (blogPostId: Int) =>
+      complete(StatusCodes.OK)
+    }
+
+  /*
+  * Type #4: "Actionable" directives
+  * */
+
+  val completeOkRoute: Route = complete(StatusCodes.OK)
+
+  val failedRoute: Route =
+    path("notSupported") {
+      failWith(new RuntimeException("Unsupported")) // completes with HTTP 500
+    }
+
+  val routeWithRejection: Route =
+    path("home") {
+      reject
+    } ~
+      path("index") {
+        completeOkRoute
+      }
+
+  val getOrPutPath: Route =
+    path("api" / "myEndpoint") {
+      get {
+        completeOkRoute
+      } ~
+      post {
+        complete(StatusCodes.Forbidden)
+      }
+    }
+
   val bindingFuture: Future[Http.ServerBinding] = Http().newServerAt("localhost", 8080).bind(queryParamExtractionRoute)
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
