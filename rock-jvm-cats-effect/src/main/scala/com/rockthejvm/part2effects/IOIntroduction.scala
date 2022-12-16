50b8fe86fd589e50cd0c84c17d3ea6b57f04227b
Tyler Schock <schock3736@gmail.com>
2022-12-15 20:06:29 -0600

    IO: Exercises :: Cats Effect :: Rock the JVM

--- rock-jvm-cats-effect/src/main/scala/com/rockthejvm/part2effects/IOIntroduction.scala
+++ rock-jvm-cats-effect/src/main/scala/com/rockthejvm/part2effects/IOIntroduction.scala
@@ -2,6 +2,7 @@ package com.rockthejvm.part2effects
 
 import cats.effect.IO
 
+import scala.annotation.tailrec
 import scala.io.StdIn
 
 object IOIntroduction {
@@ -34,9 +35,74 @@ object IOIntroduction {
   def smallProgram_v2(): IO[Unit] =
     (IO(StdIn.readLine()), IO(StdIn.readLine())).mapN(_ + _).map(println)
 
+  def sequenceTakeLast[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa.flatMap(_ => iob)
+
+  def sequenceTakeLast2[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa *> iob // andThen
+
+  def sequenceTakeLast3[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa >> iob // andThen wth by-name call
+
+  def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa.flatMap(a => iob.map(_ => a))
+
+  def sequenceTakeFirst2[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa <* iob // "<<" lazy version of "<*"
+
+
+  def forever[A](io: IO[A]): IO[A] =
+    io.flatMap(_ => forever(io))
+
+  def forever2[A](io: IO[A]): IO[A] =
+    io >> forever2(io)
+
+  def forever3[A](io: IO[A]): IO[A] =
+    io *> forever3(io) // StackOverflowError due to eager evaluation
+
+  def forever4[A](io: IO[A]): IO[A] =
+    io.foreverM // with tail recursion
+
+  def convert[A, B](ioa: IO[A], value: B): IO[B] =
+    ioa.map(_ => value)
+
+  def convert2[A, B](ioa: IO[A], value: B): IO[B] =
+    ioa.as(value) // same
+
+  def asUnit[A](ioa: IO[A]): IO[Unit] =
+    ioa.map(_ => ())
+
+  def asUnit2[A](ioa: IO[A]): IO[Unit] =
+    ioa.as(()) // discourage = don't use
+
+  def asUnit3[A](ioa: IO[A]): IO[Unit] =
+    ioa.void // same - encourage
+
+  def sum(n: Int): Int =
+    if (n <= 0) 0
+    else n + sum(n - 1)
+
+  def sumIO(n: Int): IO[Int] =
+    if (n <= 0) IO(0)
+    else for {
+      lastNumber <- IO(n)
+      prevSum <- sumIO(n - 1)
+    } yield prevSum + lastNumber
+
+  def fibonacci(n: Int): IO[BigInt] =
+    if (n < 2) IO(1)
+    else for {
+      last <- IO.defer(fibonacci(n - 1)) // same as .delay(...).flatten
+      prev <- IO.defer(fibonacci(n - 2))
+    } yield last + prev
+
+
   def main(args: Array[String]): Unit = {
     import cats.effect.unsafe.implicits.global // "platform"
     // "end of the world"
-    println(smallProgram_v2().unsafeRunSync())
+    //println(smallProgram_v2().unsafeRunSync())
+    //println(sumIO(20000).unsafeRunSync())
+    (1 to 100).foreach(i => println(fibonacci(i).unsafeRunSync()))
+    //sum(20000)
   }
 }
